9b9d8fc98199cca476440d5a9fa18268f692a778
schnekie <schnek@icloud.com>
2013-07-23 10:41:50 +0200

    Display categories filter

--- wp-content/themes/bosc/archive-portfolio.php
+++ wp-content/themes/bosc/archive-portfolio.php
@@ -10,24 +10,49 @@ get_header(); ?>
 
 <div class="container-fluid">
   <div class="row-fluid">
-    <div class="span3">
+    <div class="span12">
+      <?php
+        while ( have_posts() ) : the_post();
+          echo get_the_term_list( $post->ID, 'genre', '|') ;
+        endwhile;
+      ?>
+    </div>
 
-<?php
-    while ( have_posts() ) : the_post();
-      $images = rwmb_meta( 'portfolio_project_gallery', 'type=image&size=portfolio_thumbnail');
-      foreach($images as $image)
-      {
-?>
-        <a href="<?php the_permalink(); ?>">
-          <?php echo "<img src='{$image['url']}'  alt='{$image['alt']}'/>" ?>
-          <?php echo the_title(); ?>
-        </a>
+    <div class="span2">
+      <?php
+        while ( have_posts() ) : the_post();
+          $images = rwmb_meta( 'portfolio_project_gallery', 'type=image&size=portfolio_thumbnail');
+            foreach($images as $image)
+              {
+      ?>
+                <a href="<?php the_permalink(); ?>" >
+                  <?php echo "<img src='{$image['url']}'  alt='{$image['alt']}'/>" ?>
+                  <?php echo the_title(); ?>
+                </a>
 <?php
         break;
       }
     endwhile;
 ?>
 
+
+
+
+
+
+      </div>
     </div>
   </div>
-</div>
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+
+
+
+
+
