19088c1d681e1af2994f1da27fec754a4c129b78
Michael Baudino <michael.baudino@alpine-lab.com>
2013-07-29 18:14:22 +0200

    Remove widgets on dashboard (in admin panel)

--- wp-content/themes/bosc/custom-admin.php
+++ wp-content/themes/bosc/custom-admin.php
@@ -19,13 +19,13 @@ function check_user_role($role, $user_id = null) {
 
 
 if (check_user_role('editor')) {
+
   //
   // Remove items from menu
   //
-
   function remove_menu_items() {
     global $menu;
-    $restricted = array(__('Media'), __('Comments'), __('Contact'));
+    $restricted = array(__('Media'), __('Comments'), __('Contact'), __('Pages'), __('Tools'));
     end ($menu);
     while (prev($menu)){
       $value = explode(' ',$menu[key($menu)][0]);
@@ -34,32 +34,44 @@ if (check_user_role('editor')) {
       }
     }
   }
-
   add_action('admin_menu', 'remove_menu_items');
 
   //
   // Remove columns in posts listing
   //
-
   function custom_post_columns($defaults) {
     unset($defaults['comments']);
     unset($defaults['author']);
     return $defaults;
   }
+  add_filter('manage_posts_columns', 'custom_post_columns');
+
 
   //
   // Remove columns in pages listing
   //
-
-  add_filter('manage_posts_columns', 'custom_post_columns');
-
   function custom_pages_columns($defaults) {
     unset($defaults['comments']);
     unset($defaults['author']);
     return $defaults;
   }
-
   add_filter('manage_pages_columns', 'custom_pages_columns');
+
+
+  //
+  // Remove widgets on the dashboard
+  //
+  function disable_default_dashboard_widgets() {
+    // remove_meta_box('dashboard_right_now', 'dashboard', 'core');
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');
+    remove_meta_box('dashboard_plugins', 'dashboard', 'core');
+    // remove_meta_box('dashboard_quick_press', 'dashboard', 'core');
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');
+    remove_meta_box('dashboard_primary', 'dashboard', 'core'); // Other WordPress News
+    remove_meta_box('dashboard_secondary', 'dashboard', 'core'); // WordPress Blog
+  }
+  add_action('admin_menu', 'disable_default_dashboard_widgets');
 
 }
 
