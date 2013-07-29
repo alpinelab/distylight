3c4e9426beca8f018bbfc472f2bbdada777b5ae7
thibaultdalban <thibault.dalban@alpine-lab.com>
2013-07-29 17:45:02 +0200

    Add a slider on the home page

--- wp-content/themes/bosc/types/require.php
+++ wp-content/themes/bosc/types/require.php
@@ -6,13 +6,27 @@
 require_once(ABSPATH.'wp-content/themes/bosc/types/portfolio.php');
 
 /**
-* POUR APPELER LE CUSTOM METABOXES
+* POUR APPELER LE CUSTOM METABOXES PORTFOLIO
 */
 require_once(ABSPATH.'wp-content/themes/bosc/types/portfolio_metaboxes.php');
+
+
+/**
+* POUR APPELER LE CUSTOM POST TYPE SLIDER
+*/
+require_once(ABSPATH.'wp-content/themes/bosc/types/slider.php');
+
+/**
+* POUR APPELER LE CUSTOM METABOXES SLIDER
+*/
+require_once(ABSPATH.'wp-content/themes/bosc/types/slider_metaboxes.php');
 
 /**
 * POUR CREER DES TAILLES IMAGES PERSOS
 */
 
-if ( function_exists( 'add_image_size' ) )
-	add_image_size( 'portfolio_thumbnail', 300, 250, true);
+if (function_exists('add_image_size'))
+{
+  add_image_size('slider', 1200, 800, true);
+  add_image_size('portfolio_thumbnail', 300, 250, true);
+}
