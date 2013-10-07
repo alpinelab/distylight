d3661fe5ea8755ccab85af44b2f7c70dc4d8b218
Michael Baudino <michael.baudino@alpine-lab.com>
2013-10-07 10:52:25 +0200

    Add "Featured image" support to Portfolio custom post type

--- wp-content/themes/distylight/functions.php
+++ wp-content/themes/distylight/functions.php
@@ -52,7 +52,7 @@ function distylight_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	/**
 	 * This theme uses wp_nav_menu() in one location.
