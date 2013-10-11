324a47b73c1746e86d55e24b8e19e6a4bb0f1985
Michael Baudino <michael.baudino@alpine-lab.com>
2013-10-11 18:51:46 +0200

    Add style to contact form template

--- wp-content/themes/distylight/template-contact.php
+++ wp-content/themes/distylight/template-contact.php
@@ -19,20 +19,20 @@ get_header(); ?>
 
 <div class="row download-portfolio">
   <div class="span12">
-    <a href="#">Download our Portfolio</a>
+    <a href="#">Our Portfolio (click to download)</a>
   </div>
 </div>
 
 <div class="row contact-page">
 
-  <div class="span6"> <?
+  <div class="span6 contact-infos"> <?
     if (have_posts()) : while (have_posts()) : the_post();
       the_content();
     endwhile; endif; ?>
   </div><!-- .span6 --> <?
 
   if ($contact_form) { ?>
-    <div class="span6">
+    <div class="span6 contact-form">
       <h4>Contact us / Contactez nous / 联系我们 / Contato</h4>
       <?= do_shortcode("[contact-form-7 id=\"{$contact_form->ID}\" title=\"Contact form\"]"); ?>
     </div><!-- .span6 --> <?
