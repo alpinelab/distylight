d1ea3c0a460e16bbf9aa6fcd3a01873716a73f3e
Michael Baudino <michael.baudino@alpine-lab.com>
2013-10-15 18:59:34 +0200

    Change single post featured images to be clickable. Also, for that, add WP Lightbox 2 plugin

--- wp-content/themes/distylight/footer.php
+++ wp-content/themes/distylight/footer.php
@@ -12,6 +12,9 @@
   </div>
 </div>
 
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
 <? wp_footer(); ?>
 
 </body>
